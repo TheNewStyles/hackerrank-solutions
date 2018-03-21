c1e8600edcd87658c647853134ec9e51ce7c26b2
TheNewStyles <tyler.peterson@utah.edu>
2018-03-20 22:05:33 -0600

    Add solution

---
+++ SimpleArraySum.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution {
+
+    /*
+     * Complete the simpleArraySum function below.
+     */
+    static int simpleArraySum(int[] ar) {
+        var arSum = 0;
+        
+        foreach (var i in ar) {
+            arSum += i;
+        }       
+        
+        return arSum;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        ;
+        int result = simpleArraySum(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
